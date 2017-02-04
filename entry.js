e97037e53a88b49c87e77dcca4e9bb0e95eff3cc
root <root@mongojaredasutton.localdomain>
2017-02-04 19:35:23 +0000

    Add server.js. Modify entry.js to iterate an array to render HelloMessage\'s. Add public folder tto contain index.html and bundle.js.

--- entry.js
+++ entry.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 var HelloMessage = require('./HelloMessage.js');
+var names = ["Jared", "Allen", "Sutton"];
 
+class Messages extends React.Component {
+  render(){
+    var list = names.map((name) => (<HelloMessage key={name} name={name} />));
+    return <div> {list} </div>;
+}
+}
 
 ReactDOM.render(
-    <HelloMessage name="HumbleJ" />,
+    <Messages />,
     document.getElementById('content')
     );
