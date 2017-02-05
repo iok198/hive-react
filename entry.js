e0e32ac08a0d2c76aa9b81e78a7d1b5bde061669
root <root@mongojaredasutton.localdomain>
2017-02-05 02:52:36 +0000

    Prepare requests to mysql with entry.js. Remove server-side functions from client-side scripts.

--- entry.js
+++ entry.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 var HelloMessage = require('./HelloMessage.js');
-var names = ["Jared", "Allen", "Sutton"];
 
 class Messages extends React.Component {
   render(){
-    var list = names.map((name) => (<HelloMessage key={name} name={name} />));
+    var list = this.props.names.map((name) => (<HelloMessage key={name} name={name} />));
     return <div> {list} </div>;
 }
 }
 
-ReactDOM.render(
-    <Messages />,
+var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+          // Data reception is done, do whatever with it!
+    var parsed = JSON.parse(this.responseText);
+    var arr = parsed.map((user) => 'k' + user.firstName);
+    ReactDOM.render(
+    <Messages names={arr}/>,
     document.getElementById('content')
     );
+    }
+  };
+  xhttp.open("GET", "/query", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send();
+
--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 var express = require('express')
 var app = express()
+var connection = require('./hive-sql.js');
 
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://jaredasutton.com:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+function qur(res){
+  return function (err,rsl,fds){
+    res.send(JSON.stringify(rsl));
+}
+}
+app.get('/query',function(req, res) {
+  connection.query('SELECT * from userDirectory where entryID<3',qur(res))
 })
 
 app.listen(3000, function () {
