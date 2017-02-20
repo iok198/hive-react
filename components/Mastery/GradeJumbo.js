dd85c8477072b6bcd25ed1c49f60cc92f12df131
Jared A Sutton <jaredasutton@gmail.com>
2017-02-20 21:15:12 +0000

    Add module.exports assignment to GradeJumbo.js

--- components/Mastery/GradeJumbo.js
+++ components/Mastery/GradeJumbo.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class BDRJumbo extends React.Component {
+class GradeJumbo extends React.Component {
   render(){
       let list = "hello";
     
     return( <div id="" className="jumbotron">
               <h1>{list}</h1>
 	    </div> );
-}
+  }
 
 }
+
+module.exports = GradeJumbo;
