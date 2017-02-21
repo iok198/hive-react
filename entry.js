5177fc9da850fefd69a6a31ddb13c6795778c1b3
Jared A Sutton <jaredasutton@gmail.com>
2017-02-21 22:17:52 +0000

    Console responseText for bdrs request.

--- entry.js
+++ entry.js
@@ -7,6 +7,7 @@ var GradeJumbo = require('./components/Mastery/GradeJumbo.js')
 var xhttp = new XMLHttpRequest(); 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
       var parsed = JSON.parse(this.responseText);
       var arr = parsed.map((user) => user);
       ReactDOM.render(
