cf02ea9be223878cf8d87781f0aedddc6aa913d6
Jared A Sutton <jaredasutton@gmail.com>
2017-02-25 23:50:02 +0000

    Add student rows and panels for mastery.

--- components/Mastery/GradeJumbo.js
+++ components/Mastery/GradeJumbo.js
@@ -77,7 +77,7 @@ class GradeJumbo extends React.Component {
       }
       )}
     </MasteryStuRow>))
-    var headerRow = (<MasteryStuRow key={3813} stuData{{}} colOffset={0} > {masteryArr.map((mRecord,id) => (<MasteryPanel key={mRecord.courseStrLOID} mRecord={mRecord} colOffset={id}/>))} </MasteryStuRow>)
+    var headerRow = (<MasteryStuRow key={3813} stuData={{}} colOffset={0} > {masteryArr.map((mRecord,id) => (<MasteryPanel key={mRecord.courseStrLOID} mRecord={mRecord} colOffset={id}/>))} </MasteryStuRow>)
     return {header: headerRow, body: studentRows};
   }
 
