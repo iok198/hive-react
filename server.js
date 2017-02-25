4fa654fb1839f2cdeb16353fb0c4959b6a5bcdf0
Jared A Sutton <jaredasutton@gmail.com>
2017-02-25 18:37:29 +0000

    Remove reference to courseStr in masteryQuery.

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ function defaultQueryCallback(req,res){
 function masteryQueryCallback(req,res){
   return function (err,rsl,fds){
     if(err){console.log(err)}
-    res.send(JSON.stringify(rsl.push(req.params.courseStr)))
+    res.send(JSON.stringify(rsl))
 }
 }
 
