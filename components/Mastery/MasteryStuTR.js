8fa3eee1f13badfa816962dffb641c99970aaa88
Jared A Sutton <jaredasutton@gmail.com>
2017-02-28 16:42:33 +0000

    Remove extra 'whitespace' from MasteryStudentTR.

--- components/Mastery/MasteryStuTR.js
+++ components/Mastery/MasteryStuTR.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 class MasteryStuTR extends React.Component {
     render(){
-        return <tr id={"mastery-row-" + this.props.stuUDID } className="mastery-row" >
-                {this.props.children}
-               </tr>
+        return <tr id={"mastery-row-" + this.props.stuUDID } className="mastery-row" >{this.props.children}</tr>
     }
 }
 
--- components/Mastery/MasteryTD.js
+++ components/Mastery/MasteryTD.js
@@ -3,7 +3,6 @@ import React from 'react'
 class MasteryTD extends React.Component {
     render(){
         return <td id={"mastery-col-head-" + this.props.mRecord.courseStrLOID } className="mastery-column" style={{left: (40 + this.props.colOffset*140) + 'px'}}>
-
                       <span className="badge" ># Assessment(s)</span>
                       <span className="lo-text-span">{this.props.mRecord.LOText}</span>
                       <br />
@@ -11,7 +10,6 @@ class MasteryTD extends React.Component {
                       <span id="" className="badge badgeA">{this.props.mRecord.mcountA}</span>
                       <span id="" className="badge badgeM">{this.props.mRecord.mcountM}</span>
                       <span id="" className="badge badgeE">{this.props.mRecord.mcountE}</span>
-
                </td>
     }
 }
