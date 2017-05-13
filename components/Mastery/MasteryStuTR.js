ff75295c12f571d602e3d1200c24e994d6ef0f3d
Jared A Sutton <jaredasutton@gmail.com>
2017-05-13 19:28:38 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryStuTR.js
+++ components/Mastery/MasteryStuTR.js
@@ -8,7 +8,11 @@ class MasteryStuTR extends React.Component {
     }
     render(){
         return <tr id={"mastery-row-" + this.props.stuUDID } className="mastery-row" >
-        {[0,1,2,3,4].map((id,index) => (<MasteryStuTD key={this.courseLOIDs[id+5*this.props.page] + '-' + this.props.stuUDID} ratingData={(!!this.props.stuData[this.courseLOIDs[id+5*this.props.page]]) ? this.props.stuData[this.courseLOIDs[id+5*this.props.page]] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0, mRating0:1}} colOffset={id}/>))}
+        {[0,1,2,3,4].map((id,index) => {
+        if(!!this.courseLOIDs[id+5*this.props.page]){
+            return <MasteryStuTD key={this.courseLOIDs[id+5*this.props.page] + '-' + this.props.stuUDID} ratingData={(!!this.props.stuData[this.courseLOIDs[id+5*this.props.page]]) ? this.props.stuData[this.courseLOIDs[id+5*this.props.page]] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0, mRating0:1}} colOffset={id}/>
+            } else return null
+        })}
         </tr>
     }
 }
