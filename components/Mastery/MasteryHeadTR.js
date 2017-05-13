174581ac55ecc83f5a10aba5cfe38e8fc5ce981c
Jared A Sutton <jaredasutton@gmail.com>
2017-05-13 19:11:48 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryHeadTR.js
+++ components/Mastery/MasteryHeadTR.js
@@ -8,7 +8,7 @@ class MasteryHeadTR extends React.Component {
     }
     render(){
         return <tr id={"mastery-row-head" } className="mastery-row" >
-        {[0,1,2,3,4].map((id,index) => (<MasteryHeadTD key={this.props.masteryArr[id+5*this.props.page].courseStrLOID} mRecord={(!!this.props.masteryArr[id+5*this.props.page]) ? this.props.masteryArr[id+5*this.props.page] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0}} colOffset={id}/>))}
+        {[0,1,2,3,4].map((id,index) => (<MasteryHeadTD key={this.props.masteryArr[id+5*this.props.page].courseStrLOID} mRecord={(!!this.props.masteryArr[id+5*this.props.page]) ? this.props.masteryArr[id+5*this.props.page] : {courseStrLOID: "NA" + id, LOID: "NA" + id, LOText: "", mstudentsN: "", mstudentsA: "",mstudentsM:"",mstudentsE:"",mcountN:0, mcountA:0, mcountM:0, mcountE:0}} colOffset={id}/>))}
         </tr>
     }
 }
