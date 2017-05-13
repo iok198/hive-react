daf2d391886b36d1595f9735caea5d6bd8c40493
Jared A Sutton <jaredasutton@gmail.com>
2017-05-13 17:57:29 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryStuTR.js
+++ components/Mastery/MasteryStuTR.js
@@ -7,7 +7,7 @@ class MasteryStuTR extends React.Component {
         this.courseLOIDs = Object.keys(this.props.stuData)
     }
     render(){
-        return <tr id={"mastery-row-" + this.props.key } className="mastery-row" >
+        return <tr id={"mastery-row-" + this.props.stuUDID } className="mastery-row" >
         {[0,1,2,3,4].map((id,index) => (<MasteryStuTD key={this.courseLOIDs[id+5*this.props.page] + '-' + this.props.stuUDID} ratingData={(!!this.props.stuData[this.courseLOIDs[id+5*this.props.page]]) ? this.props.stuData[this.courseLOIDs[id+5*this.props.page]] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0, mRating0:1}} colOffset={id}/>))}
         </tr>
     }
