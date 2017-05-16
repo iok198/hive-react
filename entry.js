735f3ec731e4719fca72712e519a4f1d7617b128
Jared A Sutton <jaredasutton@gmail.com>
2017-05-16 23:58:26 +0000

    Simplify GradeJumbo.

--- entry.js
+++ entry.js
@@ -22,7 +22,7 @@ getRequestForReact("/mastery/s7..........",(arr) => ( [
   
   ])) 
   
-postRequestForReact("sendgrades/s7..........",(arr) => (null),(res)=>(console.log(res)))
+postRequestForReact("sendgrades/s7..........",function(arg){},(res)=>(console.log(res)))
 /*
 var xhttp = new XMLHttpRequest(); 
   xhttp.onreadystatechange = function() {
