4f54a79d9ae93f3e8bc6391cc270e04d0b469915
Jared A Sutton <jaredasutton@gmail.com>
2017-05-16 20:27:15 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryHeadTR.js
+++ components/Mastery/MasteryHeadTR.js
@@ -11,8 +11,8 @@ class MasteryHeadTR extends React.Component {
         <td>{"Student / Learning Outcome"}</td>
         {[0,1,2,3].map((id,index) => 
         {
-        if(!!this.props.masteryArrS[Object.keys(this.props.parsedMastery.masteryArrS)[id+4*this.props.page]]){
-        return <MasteryHeadTD key={this.props.masteryArrS[Object.keys(this.props.parsedMastery.masteryArrS)[id+4*this.props.page]].courseStrLOID} mRecord={(!!this.props.parsedMastery.masteryArrS[Object.keys(this.props.parsedMastery.masteryArrS)[id+4*this.props.page]]) ? this.props.parsedMastery.masteryArrS[Object.keys(this.props.parsedMastery.masteryArrS)[id+4*this.props.page]] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0}} colOffset={id}/>
+        if(!!this.props.masteryArrS[Object.keys(this.props.masteryArrS)[id+4*this.props.page]]){
+        return <MasteryHeadTD key={this.props.masteryArrS[Object.keys(this.props.masteryArrS)[id+4*this.props.page]].courseStrLOID} mRecord={(!!this.props.masteryArrS[Object.keys(this.props.masteryArrS)[id+4*this.props.page]]) ? this.props.masteryArrS[Object.keys(this.props.masteryArrS)[id+4*this.props.page]] : {mcountN:0, mcountA:0, mcountM:0, mcountE:0}} colOffset={id}/>
         }
             else return null
         })
