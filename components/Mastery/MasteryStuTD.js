a982bb9b0bdc17491f45d993114b0e829c2b2a6a
Jared A Sutton <jaredasutton@gmail.com>
2017-05-16 03:21:39 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryStuTD.js
+++ components/Mastery/MasteryStuTD.js
@@ -1,11 +1,18 @@
 import React from 'react'
 //possible parent: MasteryStuRow.js
+
 class MasteryStuTD extends React.Component {
+    constructor(props){
+      super(props)
+      this.state.mRating0 = this.props.ratingData.mRating0
+    }
+  
+  
     render(){
         return <td id={"mastery-col-" + this.props.ratingData.courseStrLOIDsID } className="mastery-column" >
                         <form>
                           <div className="form-group">
-                            <select className="form-control" id="sel1" defaultValue={ this.props.ratingData.mRating0 } onChange={this.props.changer(this.props.ratingData.LOID,this.props.ratingData.stuUDID)}>
+                            <select className="form-control" id="sel1" value={ this.state.mRating0 } onChange={this.props.changer(this.props.ratingData.stUDID,this.props.ratingData.LOID,this.state.mRating0)}>
                               <option value="1">Not Yet</option>
                               <option value="2">Approaching</option>
                               <option value="3">Meeting</option>
