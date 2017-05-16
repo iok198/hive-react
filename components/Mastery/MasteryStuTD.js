f68bb297e2db685408602eceae2a100ec39fce2c
Jared A Sutton <jaredasutton@gmail.com>
2017-05-16 12:22:46 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryStuTD.js
+++ components/Mastery/MasteryStuTD.js
@@ -9,8 +9,9 @@ class MasteryStuTD extends React.Component {
     }
     changeSelect(event){
       this.setState({mRating0: event.target.value})
-      this.props.changer(this.props.ratingData.stuUDID,this.props.ratingData.LOID, this.state.mRating0)()
+      
     }
+    componentDidUpdate(){this.props.changer(this.props.ratingData.stuUDID,this.props.ratingData.LOID, this.state.mRating0)()}
   
     render(){
         return <td id={"mastery-col-" + this.props.ratingData.courseStrLOIDsID } className="mastery-column" >
