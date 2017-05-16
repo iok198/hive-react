bf4f633caebf2c4ce4425e19b0c27f25bb074360
Jared A Sutton <jaredasutton@gmail.com>
2017-05-16 18:42:58 +0000

    Simplify GradeJumbo.

--- components/Mastery/MasteryStuTD.js
+++ components/Mastery/MasteryStuTD.js
@@ -9,7 +9,7 @@ class MasteryStuTD extends React.Component {
       this.changeSelectState = this.changeSelectState.bind(this)
     }
     changeSelect(event){
-      this.setState(this.changeSelectState(event.target.value),this.props.changer(this.props.ratingData.stuUDID,this.props.ratingData.LOID, this.state.mRating0,this.props.mArrKey))
+      this.setState(this.changeSelectState(event.target.value),this.props.changer(this.props.ratingData.stuUDID,this.props.ratingData.LOID, event.target.value,this.props.mArrKey))
       
     }
     
