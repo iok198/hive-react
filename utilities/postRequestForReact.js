7f1d0b2788144f028fb91b8926f3f4ed2797086a
Jared A Sutton <jaredasutton@gmail.com>
2017-05-17 00:34:18 +0000

    Simplify GradeJumbo.

--- utilities/postRequestForReact.js
+++ utilities/postRequestForReact.js
@@ -6,9 +6,9 @@ var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var text = this.responseText;
-
+      var parsed = JSON.parse(text)
       if(typeof callback == "function"){
-      callback(text)}
+      callback(parsed)}
     }
   }
   
