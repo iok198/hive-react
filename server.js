47b8549676c8a2bcd8870aee8a7e421519a07fa5
Jared A Sutton <jaredasutton@gmail.com>
2017-05-18 18:46:35 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- server.js
+++ server.js
@@ -104,10 +104,11 @@ app.get('/grades/:courseQueryStr',function(req,res){
 app.post("/sendgrades",function(req,res){
     console.log(req.body)
     var reqjson = JSON.parse(req.body)
+    if(reqjson.hasOwnProperty("string") && reqjson.hasOwnProperty("assessRatingID")){
     connection.query('UPDATE hive1617.assessmentRatings SET ratings = ? WHERE entryID = ?', [reqjson.string,reqjson.assessRatingID], function (error, results, fields) {
     if (error) throw error;
     
-    })
+    })}
     res.send(JSON.stringify(req.body))
   
   }
