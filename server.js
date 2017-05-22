cc9826b24693fb2ccf246ee5336ae9be99887b76
Jared A Sutton <jaredasutton@gmail.com>
2017-05-22 01:03:01 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.get('/bdrs',function(req, res) {
 })
 
 app.get('/users',function(req, res){
-  if(req.user){console.log('got a user')
-    console.log(req.user)}
+  if(req){console.log('got a user')
+    console.log(req)}
   connection.query('SELECT * FROM userDirectory where entryID=1',usersQueryCallback(req,res))
 })
 
