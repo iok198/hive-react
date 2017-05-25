090c2647e4f940837166563dea9e20e531b51184
Jared A Sutton <jaredasutton@gmail.com>
2017-05-25 19:16:26 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/Mastery/MasteryJumbo.js
+++ components/Mastery/MasteryJumbo.js
@@ -26,6 +26,9 @@ class MasteryJumbo extends React.Component {
       //let list = this._getMastery(this.state.parsedMastery);
     
     return( <div className="jumbotron">
+        <ul className="nav nav-pills">
+            {Object.keys(this.props.user.stuCourseQuObj.strObj).map((key,id)=> (<li><a href="#" onClick={function(event){event.preventDefault();}}>{this.props.user.stuCourseQuObj.strObj[key]}</a></li>))}
+        </ul>
         {(this.state.mArr.length > 0) ? <MasteryContainer mArr={this.state.mArr} /> : null}
 	    </div> );
   }
