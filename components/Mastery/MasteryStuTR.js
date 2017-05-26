c8a40e5fb3493cc24aeb8f4940ba8fa24526e028
Jared A Sutton <jaredasutton@gmail.com>
2017-05-26 01:17:39 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/Mastery/MasteryStuTR.js
+++ components/Mastery/MasteryStuTR.js
@@ -13,6 +13,7 @@ class MasteryStuTR extends React.Component {
         {[0,1,2,3].map((id,index) => {
         if(!!this.courseLOIDs[id+4*this.props.page] ){
             console.log(this.props.stuData)
+            console.log(id+4*this.props.page)
             return <MasteryStuTD key={this.courseLOIDs[id+4*this.props.page] + '-' + this.props.stuUDID} mArrKey={!!this.props.stuData[this.courseLOIDs[id+4*this.props.page]] ? this.props.stuData[this.courseLOIDs[id+4*this.props.page]].mArrKey : -1} ratingData={!!this.props.stuData[this.courseLOIDs[id+4*this.props.page]] ? this.props.stuData[this.courseLOIDs[id+4*this.props.page]] : {mcountA:0,mcountE:0,mcountM:0,mcountN:0,mRating0:1}} colOffset={id} changer={this.props.changer}/>
             } else return null
         })}
