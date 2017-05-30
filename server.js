32b54c28e51758c3db3773d44b4a114eaf8572b0
Jared A Sutton <jaredasutton@gmail.com>
2017-05-30 20:51:49 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ passport.use(new GoogleStrategy(googPassCred,
          //done(err,rsl[0])
          if(err) throw err
          if(rsl.length > 0){
+           console.log('rsl: ')
          console.log(rsl)
          done(null,rsl[0])} else { done(null,extractProfile(profile))}
          
