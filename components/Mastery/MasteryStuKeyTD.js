d7d595d85c3226b42779d00cb68b0ca29a95b89b
Jared A Sutton <jaredasutton@gmail.com>
2017-06-01 16:04:08 +0000

    Put numerical score in stuKey.

--- components/Mastery/MasteryStuKeyTD.js
+++ components/Mastery/MasteryStuKeyTD.js
@@ -5,7 +5,7 @@ class MasteryStuKeyTD extends React.Component {
         return <td id={"mastery-col-key" + this.props.stuUDID } className="mastery-column" >
                       <span>{this.props.stuBio.name}</span>
                       <br />
-                      <span id="" className="label">{(0*this.props.mRating0s[1] + 
+                      <span id="" className="label label-default">{(0*this.props.mRating0s[1] + 
                       0.65*this.props.mRating0s[2] + 1*this.props.mRating0s[3] + 1.2*this.props.mRating0s[4])/(this.props.mRating0s[1] + 
                       this.props.mRating0s[2] + this.props.mRating0s[3] + this.props.mRating0s[4])}</span>
                       <br />
