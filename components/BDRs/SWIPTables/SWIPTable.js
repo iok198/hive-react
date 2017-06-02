ee9638d2a3b3928c43e4825af1fd4e580b442c19
Jared A Sutton <jaredasutton@gmail.com>
2017-06-02 02:55:39 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/BDRs/SWIPTables/SWIPTable.js
+++ components/BDRs/SWIPTables/SWIPTable.js
@@ -18,7 +18,7 @@ class SWIPContainer extends React.Component {
       </tr>
     </thead>
     <tbody>
-      {Object.keys(this.props.swipRows).map((swipRow) => (<tr>
+      {Object.keys(this.props.swipRows).map((swipRow) => (<tr key={swipRow.stuUDID}>
             <td>{swipRow.name}</td><td>{swipRow.classNo}</td><td>{swipRow.swips}</td>
         </tr>))}
     </tbody>
