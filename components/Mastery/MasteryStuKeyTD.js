1ea401ea06083ba3f866bb8acfd3e15763c3502f
Jared A Sutton <jaredasutton@gmail.com>
2017-06-02 05:03:27 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/Mastery/MasteryStuKeyTD.js
+++ components/Mastery/MasteryStuKeyTD.js
@@ -4,7 +4,7 @@ class MasteryStuKeyTD extends React.Component {
     render(){
         return <td id={"mastery-col-key" + this.props.stuUDID } className="mastery-column" >
                       <span>{this.props.stuBio.name}</span>
-                      <br />
+
                       <span id="" className="label label-default">{Math.round(100*(0*this.props.mRating0s[1] + 
                       0.65*this.props.mRating0s[2] + 1*this.props.mRating0s[3] + 1.2*this.props.mRating0s[4])/(this.props.mRating0s[1] + 
                       this.props.mRating0s[2] + this.props.mRating0s[3] + this.props.mRating0s[4]))}</span>
