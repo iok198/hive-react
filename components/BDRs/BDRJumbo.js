3bce7e0129a3c6c36e34fa481912494a0d398efa
Jared A Sutton <jaredasutton@gmail.com>
2017-06-02 04:08:48 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/BDRs/BDRJumbo.js
+++ components/BDRs/BDRJumbo.js
@@ -10,6 +10,7 @@ class BDRJumbo extends React.Component {
 
     this.state = {showBDRs: false,swipArr:[]}
     this.getSWIPsForThreshold = this.getSWIPsForThreshold.bind(this)
+    
    }
   
   getSWIPsForThreshold(swipThreshold){
@@ -21,6 +22,7 @@ class BDRJumbo extends React.Component {
   changeSWIPArrState(arr){
     this.setState({swipArr:arr})
   }
+  
   
   componentWillMount(){this.getSWIPsForThreshold("le20")()}
   
@@ -36,6 +38,12 @@ class BDRJumbo extends React.Component {
     
     return( <div id="" className="jumbotron">
               <button type="button" className="btn btn-primary" onClick={this._handleClick.bind(this)}>{buttonText}</button>
+              <select className="form-control" id="sel1" onChange={console.log}>
+                <option value={"le20"}>{"All"}</option>
+                <option value={"le13"}>{"<=13"}</option>
+                <option value={"ee15"}>{"15"}</option>
+                <option value={"gt12"}>{">12"}</option>
+              </select>
               <br />
               <SWIPContainer swipRows={this.state.swipArr}/>
               {list}
