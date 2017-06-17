e20aa09916e60a6d58d1af0630c48f83c6ee4009
Jared A Sutton <jaredasutton@gmail.com>
2017-06-17 15:51:44 +0000

    Fishing for bug that causes no save on mRating0 == 0.

--- components/Mastery/MasteryStuKeyTD.js
+++ components/Mastery/MasteryStuKeyTD.js
@@ -4,15 +4,13 @@ class MasteryStuKeyTD extends React.Component {
     render(){
         return <td id={"mastery-col-key" + this.props.stuUDID } className="mastery-column" >
                       <span>{this.props.stuBio.name}<sup>{this.props.stuUDID}</sup></span>
-
-                      <span id="" className="label label-default">{Math.round(100*(0*this.props.mRating0s[1] + 
-                      0.65*this.props.mRating0s[2] + 1*this.props.mRating0s[3] + 1.2*this.props.mRating0s[4])/(this.props.mRating0s[1] + 
-                      this.props.mRating0s[2] + this.props.mRating0s[3] + this.props.mRating0s[4]))}</span>
                       <br />
                       <span id="" className="badge badgeNY" >{this.props.mRating0s[1]}</span>
                       <span id="" className="badge badgeA">{this.props.mRating0s[2]}</span>
                       <span id="" className="badge badgeM">{this.props.mRating0s[3]}</span>
-                      <span id="" className="badge badgeE">{this.props.mRating0s[4]}</span>
+                      <span id="" className="badge badgeE">{this.props.mRating0s[4]}</span> | <span id="" className="label label-default">{Math.round(100*(0*this.props.mRating0s[1] + 
+                      0.65*this.props.mRating0s[2] + 1*this.props.mRating0s[3] + 1.2*this.props.mRating0s[4])/(this.props.mRating0s[1] + 
+                      this.props.mRating0s[2] + this.props.mRating0s[3] + this.props.mRating0s[4]))}</span>
                </td>
     }
 }
