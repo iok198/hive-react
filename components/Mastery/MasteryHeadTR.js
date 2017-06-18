567317a5ab19eed73f004c56fd7c1d5e04bb8d15
Jared A Sutton <jaredasutton@gmail.com>
2017-06-18 19:33:30 +0000

    Fix reference to postRequestForReact in GradeJumbo

--- components/Mastery/MasteryHeadTR.js
+++ components/Mastery/MasteryHeadTR.js
@@ -9,10 +9,16 @@ class MasteryHeadTR extends React.Component {
     render(){
         return <tr id={"mastery-row-head" } className="mastery-row" >
         <td><div className="panel panel-default"><div className="panel-body">{"Student"}
-            <div className="btn-group-vertical" role="group" aria-label="...">
-              <button type="button" className="btn btn-default" onClick={this.props.upVPage}><span className="glyphicon glyphicon-arrow-up" ></span></button>
-              <button type="button" className="btn btn-default" onClick={this.props.downVPage}><span className="glyphicon glyphicon-arrow-down" ></span></button>
-            </div></div></div>
+                    <div className="btn-group-vertical" role="group" aria-label="...">
+                      <button type="button" className="btn btn-default" onClick={this.props.upVPage}><span className="glyphicon glyphicon-arrow-up" ></span></button>
+                      <button type="button" className="btn btn-default" onClick={this.props.downVPage}><span className="glyphicon glyphicon-arrow-down" ></span></button>
+                    </div>
+                    <div className="input-group">
+                      <span className="input-group-addon" id="basic-addon3">Name:</span>
+                      <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+                    </div>
+                </div>
+            </div>
             <div className="btn-group pager">
                 <button type="button" className="btn btn-default"  onClick={this.props.prevPage}><span className="glyphicon glyphicon-arrow-left" ></span></button>
                 <button type="button" className="btn btn-default"  onClick={this.props.nextPage}><span className="glyphicon glyphicon-arrow-right" ></span></button>
