6e8b1de15e97ad2b83f0ad6be92a4e8b2d79a29f
Jared A Sutton <jaredasutton@gmail.com>
2017-08-18 12:52:05 -0400

    Untrack the locally relevant files.

--- googPassCred.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'clientID' : '494745762476-omrenqp1gk1qr3vlmu6111p930pfreud.apps.googleusercontent.com',
-	'clientSecret' : '1W1sa2mJcnEWJYrthiouQcqw',
-	'callbackURL' : 'http://localhost:3000/authd/callback'
-}
