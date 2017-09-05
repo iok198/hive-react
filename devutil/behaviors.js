4dd52a5831ecd412f8911859d185f1f88186e7c6
Jared A Sutton <jaredasutton@gmail.com>
2017-09-05 08:26:39 -0400

    Add devutil.

---
+++ devutil/behaviors.js
@@ -0,0 +1,7 @@
+module.exports = {
+            "Inappropriate Language":1, "Unexcused Lateness":1, "Inappropriate Physical Contact":1, "Property Misuse (non-threatening)":1,
+             "Inappropriate Personal Tech Use":1, "Out of Uniform w/Access":1, "Class Disruption/Non-compliant":1, "Other (Please specify in \"Incident Description.\")":1,
+             "Verbal Abuse":2,
+             "Skipping Class/Cutting":2, "Unauthorized Building Exit":2, "Fighting/Physical Aggression":2, "Property Misuse (threatening)":2, "Property Damage/Vandalism":2,
+             "Cheating/Lying":2, "Bullying/Harrassment":2, "Offensive Sexual Conduct":2, "Behavioral Insubordination":2, "Theft/Forgery":2, "Tobacco/Alcohol/Drugs":2,
+             "Combustible/Arson/Bomb Threat":2, "Weapons":2};
