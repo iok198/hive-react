4a332af2728486d405d151e1a540ec38dfbdaeaf
Jared A Sutton <jaredasutton@gmail.com>
2017-09-12 09:34:44 -0400

    Add repair ticket link.

--- components/Greeting/GreetingJumbo.js
+++ components/Greeting/GreetingJumbo.js
@@ -8,7 +8,7 @@ class GreetingJumbo extends React.Component {
     return( <div id="" className="jumbotron">
               <div><img height="50" src="./public/img/hivelogo.png"/></div>
               <h1>Hello {name + " " + this.props.user.lastName}</h1>
-              {this.props.user.courseStr.substring(0,1) != 's' ? (<p><a href="https://sites.google.com/a/ms442.org/the-hub">Visit the Hub</a></p>) : (<a href="https://docs.google.com/forms/d/e/1FAIpQLSf3LAsfrbyfJLvE55AtkOE4W2BJV4rXnb0CACBMJ4aDIbtHLw/viewform?usp=sf_link"><img src="http://www.freeiconspng.com/uploads/wrench-icon-3.png" width=60/></a>)}
+              {(this.props.user.courseStr.substring(0,1) != 's') ? (<p><a href="https://sites.google.com/a/ms442.org/the-hub">{"Visit the Hub"}</a></p>) : (<a href="https://docs.google.com/forms/d/e/1FAIpQLSf3LAsfrbyfJLvE55AtkOE4W2BJV4rXnb0CACBMJ4aDIbtHLw/viewform?usp=sf_link"><img src="http://www.freeiconspng.com/uploads/wrench-icon-3.png" /></a>)}
 	    </div> );
   }
 
