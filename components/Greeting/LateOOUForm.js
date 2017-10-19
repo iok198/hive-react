e7159b361d90846c7be891cce52977b27078c896
Jared A Sutton <jaredasutton@gmail.com>
2017-10-19 14:04:34 -0400

    Fix swipCode reference in LateOOUForm.

--- components/Greeting/LateOOUForm.js
+++ components/Greeting/LateOOUForm.js
@@ -13,27 +13,32 @@ class LateOOUForm extends React.Component {
                     {this.props.students.map((student,id) => {
                       var buttons = null
                       if(!student.problemBehavior){
-                        buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ')'}</td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Late Arrival: ')}.bind(this)}>Late</span></td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Out of Uniform Arrival: ')}.bind(this)}>Out of Uniform</span></td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Late & Out of Uniform Arrival: ')}.bind(this)}>Late & Out of Uniform</span></td></tr>)} else {
-                          if(student.problemBehavior.substring(0,31) == 'Late & Out of Uniform Arrival: '){
-                            if(student.swipCode == 2){
-                            buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.upgradeLateOOU(student.entryID,"Student attended same-day Lunch Detention.",student.bdrID)}.bind(this)}>Attended Lunch Detention</span></td><td><span className="btn btn-info" onClick={function(){this.props.upgradeLateOOU(student.entryID,'Student attended same-day Late Dismissal.',student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>)}
-                            else if (student.swipCode < 0) {
-                              <tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td>Attended same-day Lunch Detention & Late Dismissal</td></tr>
-                            }
-                            else if (student.commentText = 'Student attended same-day Lunch Detention.') {
-                              buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Late Dismissal.",student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>)
-                            } else if (student.commentText = 'Student attended same-day Late Dismissal.'){
-                                buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Lunch Detention.",student.bdrID)}.bind(this)}>Attended Lunch Detention</span></td></tr>)
-                            }
+                        buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ')'}</td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Late Arrival: ')}.bind(this)}>Late</span></td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Out of Uniform Arrival: ')}.bind(this)}>Out of Uniform</span></td><td><span className="btn btn-info" onClick={function(){this.props.sendLateOOU(student.entryID,'Late & Out of Uniform Arrival: ')}.bind(this)}>Late & Out of Uniform</span></td></tr>)
+                      }
+                      else {
+                        if(student.problemBehavior.substring(0,31) == 'Late & Out of Uniform Arrival: '){
+                          if(parseInt(student.swipCode) == 2){
+                            buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.upgradeLateOOU(student.entryID,"Student attended same-day Lunch Detention.",student.bdrID)}.bind(this)}>Attended Lunch Detention</span></td><td><span className="btn btn-info" onClick={function(){this.props.upgradeLateOOU(student.entryID,'Student attended same-day Late Dismissal.',student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>)
                           }
-                          else {
-                             if (student.swipCode < 0) {
-                              <tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td>Attended same-day Late Dismissal</td></tr>
-                            }
-                            else {
-                            buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') only 1'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Late Dismissal.",student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>) }
+                          else if (parseInt(student.swipCode) < 0) {
+                            <tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td>Attended same-day Lunch Detention & Late Dismissal</td></tr>
+                          }
+                          else if (student.commentText = 'Student attended same-day Lunch Detention.') {
+                            buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Late Dismissal.",student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>)
+                          }
+                          else if (student.commentText = 'Student attended same-day Late Dismissal.'){
+                              buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Lunch Detention.",student.bdrID)}.bind(this)}>Attended Lunch Detention</span></td></tr>)
                           }
                         }
+                        else {
+                          if (parseInt(student.swipCode) < 0) {
+                            <tr key={id}><td>{student.name + ' (' + student.classNo + ') late & oou'}</td><td>Attended same-day Late Dismissal</td></tr>
+                          }
+                          else {
+                          buttons = (<tr key={id}><td>{student.name + ' (' + student.classNo + ') only 1'}</td><td><span className="btn btn-info" onClick={function(){this.props.restoreLateOOU(student.entryID,"Student attended same-day Late Dismissal.",student.bdrID)}.bind(this)}>Attended Late Dismissal</span></td></tr>)
+                          }
+                        }
+                      }
                       return buttons
                       })}
                     </tbody>
